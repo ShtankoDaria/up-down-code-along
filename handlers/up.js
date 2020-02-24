185c475e386d6929ee7260f42287b98574eddfa2
Daria <daria.shtanko@gmail.com>
2020-02-24 07:58:47 +0100

    first changes

--- handlers/up.js
+++ handlers/up.js
@@ -2,15 +2,24 @@ function upHandler() {
   debugger;
   // read user input & cast to Number
 
+  const increment = document.getElementById("current-number").innerHTML;
+
   // read state
+  const oldCurrent = numbers.current;
 
   // perform logic, combining state with user input
   const newCurrent = oldCurrent + increment;
 
   // update state (.current & .past)
-
+  numbers.current = newCurrent;
+  numbers.past.push(numbers.current);
   // render to user
-
+  document.getElementById("current-number").innerHTML = numbers.current;
   // log user interaction
+
   //  story name, increment, newCurrent, copy of state
+  log.push({
+    handler: "up",
+    numbers: JSON.parse(JSON.stringify(numbers))
+  });
 }
--- init.js
+++ init.js
@@ -1,6 +1,11 @@
 window.onload = () => {
   debugger;
   // log initial state
-
+  window.onload = () => {
+    // log initial state
+    log.push({ initialNumbers: JSON.parse(JSON.stringify(numbers)) });
+    // render UI from initial state
+    document.getElementById("current-number").innerHTML = numbers.current;
+  };
   // render initial UI from state
-}
+};
